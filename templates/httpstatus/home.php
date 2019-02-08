f65db83f6974c3607b5ca9a96476ca04aaee8192
quentin <quentin.gans@ynov.com>
2019-02-08 17:36:13 +0100

    slight update

--- templates/httpstatus/home.php
+++ templates/httpstatus/home.php
@@ -1,10 +1,5 @@
 <?php include(PWD_TEMPLATES . '/incs/head.php'); ?>
 
-<<<<<<< HEAD
-<p>Home</p>
-=======
 <?php include(PWD_TEMPLATES . '/incs/header.php'); ?>
-
->>>>>>> a868e370c4e77cff6851240db331ff85953d7ff2
 
 <?php include(PWD_TEMPLATES . '/incs/footer.php'); ?>

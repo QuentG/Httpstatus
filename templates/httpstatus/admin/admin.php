733dae864084a60c9349d95d46fffbf39891f768
lucasconsejo <lucas.consejo@ynov.com>
2019-02-10 21:34:47 +0100

    update check site home/admin

--- templates/httpstatus/admin/admin.php
+++ templates/httpstatus/admin/admin.php
@@ -5,31 +5,37 @@
     <div class="row">
         <h3>List of sites</h3>
         
-        <table class="table mt-2">
-            <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Url</th>
-                    <th scope="col">Status</th>
-                    <th scope="col">Historic</th>
-                    <th scope="col">Edit</th>
-                    <th scope="col">Delete</th>
-                </tr>
-            </thead>
-
-            <tbody>
-                <?php foreach($sites as $key => $site) { ?>
+        <?php if(empty($sites)){?>
+            <div id="no-sites" class="col-md-12">
+                <h4>No sites to display</h4>
+            </div>
+        <?php }else{ ?>
+            <table class="table mt-2">
+                <thead>
                     <tr>
-                        <th scope="row"><?= $site['id'] ?></th>
-                        <td><a href="<?= $site['url_site'] ?>" target="_blank"><?= $site['url_site'] ?></a></td>
-                        <td>HTTP : <?= $site['status_site']?></td>
-                        <td><a href="./show/<?= $site['id'] ?>">See the history</a></td>
-                        <td><a href="./admin/edit">Edit</a></td>
-                        <td><a href="./admin?delete=<?= $site['id'] ?>">Delete</a></td>
+                        <th scope="col">ID</th>
+                        <th scope="col">Url</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Historic</th>
+                        <th scope="col">Edit</th>
+                        <th scope="col">Delete</th>
                     </tr>
-                <?php } ?>
-            </tbody>
-        </table>
+                </thead>
+
+                <tbody>
+                    <?php foreach($sites as $key => $site) { ?>
+                        <tr>
+                            <th scope="row"><?= $site['id'] ?></th>
+                            <td><a href="<?= $site['url_site'] ?>" target="_blank"><?= $site['url_site'] ?></a></td>
+                            <td>HTTP : <?= $site['status_site']?></td>
+                            <td><a href="./show/<?= $site['id'] ?>">See the history</a></td>
+                            <td><a href="./admin/edit">Edit</a></td>
+                            <td><a href="./admin?delete=<?= $site['id'] ?>">Delete</a></td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        <?php } ?>
     </div>  
 </div>
 
--- templates/httpstatus/home.php
+++ templates/httpstatus/home.php
@@ -4,27 +4,33 @@
 
 <div class="container">
     <div class="row">  
-        <table class="table mt-2">
-            <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Url</th>
-                    <th scope="col">Status</th>
-                    <th scope="col">Historic</th>
-                </tr>
-            </thead>
-
-            <tbody>
-                <?php foreach($sites as $key => $site) { ?>
+        <?php if(empty($sites)){?>
+            <div id="no-sites" class="col-md-12">
+                <h4>No sites to display</h4>
+            </div>
+        <?php }else{ ?>
+            <table class="table mt-2">
+                <thead>
                     <tr>
-                        <th scope="row"><?= $site['id'] ?></th>
-                        <td><a href="<?= $site['url_site'] ?>" target="_blank"><?= $site['url_site'] ?></a></td>
-                        <td>HTTP : <?= $site['status_site']?></td>
-                        <td><a href="./show/<?= $site['id'] ?>">See the history</a></td>
+                        <th scope="col">ID</th>
+                        <th scope="col">Url</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Historic</th>
                     </tr>
-                <?php } ?>
-            </tbody>
-        </table>
+                </thead>
+
+                <tbody>
+                    <?php foreach($sites as $key => $site) { ?>
+                        <tr>
+                            <th scope="row"><?= $site['id'] ?></th>
+                            <td><a href="<?= $site['url_site'] ?>" target="_blank"><?= $site['url_site'] ?></a></td>
+                            <td>HTTP : <?= $site['status_site']?></td>
+                            <td><a href="./show/<?= $site['id'] ?>">See the history</a></td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        <?php } ?>
     </div>
 </div> 
 
